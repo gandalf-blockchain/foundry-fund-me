14ff8642e80aa35de194b48b8a88a3f1fe33a7ec
gandalf-blockchain <gandalfblockchain@gmail.com>
2026-01-22 00:04:02 +0700

    Refactoring script agar dibacanya mudah dan menanamkan mindset attacker if script di deploy 2 kali efek nya apa?

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
@@ -7,6 +6,9 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
+
+    uint8 private constant DECIMALS = 8;
+    int256 private constant INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         address priceFeed;
@@ -23,27 +25,35 @@ contract HelperConfig is Script {
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaConfig = NetworkConfig({
+        return NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
-        return sepoliaConfig;
     }
 
     function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory ethConfig = NetworkConfig({
+        return NetworkConfig({
             priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
         });
-        return ethConfig;
     }
 
     function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        // Idempotent check: If it already exists, don't redeploy it.
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
         vm.stopBroadcast();
 
         NetworkConfig memory anvilConfig = NetworkConfig({
             priceFeed: address(mockPriceFeed)
         });
+
+        activeNetworkConfig = anvilConfig;
         return anvilConfig;
     }
 }
